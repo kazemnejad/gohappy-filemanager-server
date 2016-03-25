124a042b1c2e70107bb11dff17db9ce80f46fec2
AmirHosein KazemNejad <ub.maka@gmail.com>
2016-03-25 19:11:28 +0430

    Add Authentication response code

--- gohappyserver/status.py
+++ gohappyserver/status.py
@@ -3,3 +3,11 @@ class ResponseCode:
     FAILED = 1
 
     BAD_REQUEST = 3
+
+
+class AuthenticationResponse:
+    UN_AUTHENTICATED_USER = 30
+    INVALID_SOURCE = 31
+    PERMISSION_DENIED = 32
+
+
