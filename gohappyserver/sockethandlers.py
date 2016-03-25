f7dd7e1e644636c2560dad4b3c1664db0204aea5
AmirHosein KazemNejad <ub.maka@gmail.com>
2016-03-25 20:15:33 +0430

    Extract hard coded fields from socket handlers

--- gohappyserver/sockethandlers.py
+++ gohappyserver/sockethandlers.py
@@ -12,11 +12,11 @@ from gohappyserver.status import ResponseCode, AuthenticationResponse, Explorati
 
 @socketio.on(ServerEvents.NEW_CONNECTION)
 def handle_new_connection(data):
-    if data is None or "token" not in data:
+    if data is None or EventFields.TOKEN not in data:
         disconnect()
         return
 
-    token = data.get("token", "")
+    token = data.get(EventFields.TOKEN, "")
     sid = request.sid
 
     user = User.get_user_by_auth_token(token)
@@ -27,7 +27,7 @@ def handle_new_connection(data):
     user.attach_new_socket(sid)
     db_session.commit()
 
-    emit(ClientEvents.CONNECTION_ESTABLISHED, {"result": ResponseCode.SUCCESSFUL}, room=sid)
+    emit(ClientEvents.CONNECTION_ESTABLISHED, {EventFields.RESULT: ResponseCode.SUCCESSFUL}, room=sid)
 
 
 @socketio.on(ServerEvents.NEW_EXPLORATION)
@@ -35,14 +35,14 @@ def handle_start_new_exploration(data):
     sid = request.sid
 
     if data is None \
-            or "token" not in data \
-            or "source" not in data:
+            or EventFields.TOKEN not in data \
+            or EventFields.SOURCE not in data:
         simple_response(ExplorerEvents.NEW_EXPLORATION_RESULT, ResponseCode.FAILED,
                         ResponseCode.BAD_REQUEST, sid)
         return
 
-    explorer = User.get_user_by_auth_token(data.get("token", ""))
-    source = User.query.filter_by(username=data.get("source", "")).first()
+    explorer = User.get_user_by_auth_token(data.get(EventFields.TOKEN, ""))
+    source = User.query.filter_by(username=data.get(EventFields.SOURCE, "")).first()
     if explorer is None:
         simple_response(ExplorerEvents.NEW_EXPLORATION_RESULT, ResponseCode.FAILED,
                         AuthenticationResponse.UN_AUTHENTICATED_USER, sid)
@@ -68,24 +68,25 @@ def handle_start_new_exploration(data):
     session.enabled = False
     db_session.commit()
 
-    emit(SourceEvents.ASK_FOR_PERMISSION, {"explorer": explorer.username, "session_id": session.uuid},
+    emit(SourceEvents.ASK_FOR_PERMISSION,
+         {EventFields.EXPLORER: explorer.username, EventFields.SESSION_ID: session.uuid},
          room=source.socket_id)
 
 
 @socketio.on(ServerEvents.EXPLORATION_PERMISSION_REQUEST_ANSWER)
 def handle_source_permission_answer(data):
     if data is None \
-            or "session_id" not in data \
-            or "token" not in data \
-            or "answer" not in data:
+            or EventFields.SESSION_ID not in data \
+            or EventFields.TOKEN not in data \
+            or EventFields.ANSWER not in data:
         return
 
-    session = Session.query.filter_by(uuid=data.get("session_id")).first()
+    session = Session.query.filter_by(uuid=data.get(EventFields.SESSION_ID)).first()
     if session is None:
         return
 
     source = session.source
-    if source is None or source.username != User.get_user_by_auth_token(data.get("token")).username:
+    if source is None or source.username != User.get_user_by_auth_token(data.get(EventFields.TOKEN)).username:
         return
 
     explorer = session.explorer
@@ -94,7 +95,7 @@ def handle_source_permission_answer(data):
             or explorer.socket_id is None:
         return
 
-    answer = data.get("answer")
+    answer = data.get(EventFields.ANSWER)
     if answer not in [ExplorationResponse.ANSWER_PERMISSION_GRANTED, ExplorationResponse.ANSWER_PERMISSION_DENIED]:
         simple_response(ExplorerEvents.NEW_EXPLORATION_RESULT,
                         ResponseCode.FAILED, ExplorationResponse.INVALID_ANSWER,
@@ -108,16 +109,17 @@ def handle_source_permission_answer(data):
         result = ResponseCode.SUCCESSFUL
         session.enabled = True
 
-        response["session_id"] = session.uuid
-        response["source"] = source.username
+        response[EventFields.SESSION_ID] = session.uuid
+        response[EventFields.SOURCE] = source.username
 
     db_session.commit()
-    response["result"] = result
-    response["answer"] = answer
-    response["message"] = answer
+    response[EventFields.RESULT] = result
+    response[EventFields.ANSWER] = answer
+    response[EventFields.MESSAGE] = answer
 
     emit(ExplorerEvents.NEW_EXPLORATION_RESULT, response, room=explorer.socket_id)
-    emit(SourceEvents.EXPLORATION_STARTED, {"session_id": session.uuid, "explorer": explorer.username},
+    emit(SourceEvents.EXPLORATION_STARTED,
+         {EventFields.SESSION_ID: session.uuid, EventFields.EXPLORER: explorer.username},
          room=source.socket_id)
 
 
@@ -126,15 +128,15 @@ def handle_path_request(data):
     sid = request.sid
 
     if data is None \
-            or "token" not in data \
-            or "session_id" not in data \
-            or "path" not in data \
-            or "request_code" in data:
+            or EventFields.TOKEN not in data \
+            or EventFields.SESSION_ID not in data \
+            or EventFields.PATH not in data \
+            or EventFields.REQUEST_CODE in data:
         simple_response(ExplorerEvents.PATH_REQUEST_RESPONSE, ResponseCode.FAILED,
                         ResponseCode.BAD_REQUEST, sid)
         return
 
-    session = Session.query.filter_by(uuid=data.get("session_id")).first()
+    session = Session.query.filter_by(uuid=data.get(EventFields.SESSION_ID)).first()
     if session is None \
             or not session.enabled \
             or session.explorer is None \
@@ -143,7 +145,7 @@ def handle_path_request(data):
                         ExplorationResponse.INVALID_SESSION, sid)
         return
 
-    explorer = User.get_user_by_auth_token(data.get("token"))
+    explorer = User.get_user_by_auth_token(data.get(EventFields.TOKEN))
     if explorer is None \
             or explorer.socket_id is None \
             or explorer.socket_id != sid \
@@ -158,7 +160,8 @@ def handle_path_request(data):
         return
 
     emit(SourceEvents.PATH_REQUESTED,
-         {"request_code": data.get("request_code"), "path": data.get("path"), "session_id": data.get("session_id")},
+         {EventFields.REQUEST_CODE: data.get(EventFields.REQUEST_CODE), EventFields.PATH: data.get(EventFields.PATH),
+          EventFields.SESSION_ID: data.get(EventFields.SESSION_ID)},
          room=session.source.socket_id)
 
 
@@ -167,15 +170,15 @@ def handle_path_request_response(data):
     sid = request.sid
 
     if data is None \
-            or "token" not in data \
-            or "request_code" not in data \
-            or "session_id" not in data \
-            or "response_data" not in data:
+            or EventFields.TOKEN not in data \
+            or EventFields.REQUEST_CODE not in data \
+            or EventFields.SESSION_ID not in data \
+            or EventFields.RESPONSE_DATA not in data:
         simple_response(SourceEvents.PATH_RESPONSE_ERROR, ResponseCode.FAILED,
                         ResponseCode.BAD_REQUEST, sid)
         return
 
-    session = Session.query.filter_by(uuid=data.get("session_id")).first()
+    session = Session.query.filter_by(uuid=data.get(EventFields.SESSION_ID)).first()
     if session is None \
             or not session.enabled \
             or session.explorer is None \
@@ -184,7 +187,7 @@ def handle_path_request_response(data):
                         ExplorationResponse.INVALID_SESSION, sid)
         return
 
-    source = User.get_user_by_auth_token(data.get("token"))
+    source = User.get_user_by_auth_token(data.get(EventFields.TOKEN))
     if source.username != session.source.username \
             or source.socket_id is None \
             or source.socket_id != sid:
@@ -199,8 +202,11 @@ def handle_path_request_response(data):
         return
 
     emit(ExplorerEvents.PATH_REQUEST_RESPONSE,
-         {"request_code": data.get("request_code"), "session_id": data.get("session_id"),
-          "response_data": data.get("response_data")},
+         {
+             EventFields.REQUEST_CODE: data.get(EventFields.REQUEST_CODE),
+             EventFields.SESSION_ID: data.get(EventFields.SESSION_ID),
+             EventFields.RESPONSE_DATA: data.get(EventFields.RESPONSE_DATA)
+         },
          room=explorer.socket_id)
 
 
@@ -209,13 +215,13 @@ def handle_close_session(data):
     sid = request.sid
 
     if data is None \
-            or "token" not in data \
-            or "session_id" not in data:
+            or EventFields.TOKEN not in data \
+            or EventFields.SESSION_ID not in data:
         simple_response(ClientEvents.SESSION_CLOSE_ERROR, ResponseCode.FAILED,
                         ResponseCode.BAD_REQUEST, sid)
         return
 
-    session = Session.query.filter_by(uuid=data.get("session_id")).first()
+    session = Session.query.filter_by(uuid=data.get(EventFields.SESSION_ID)).first()
     if session is None \
             or session.closed \
             or session.source is None \
@@ -224,7 +230,7 @@ def handle_close_session(data):
                         ExplorationResponse.INVALID_SESSION, sid)
         return
 
-    user = User.get_user_by_auth_token(data.get("token"))
+    user = User.get_user_by_auth_token(data.get(EventFields.TOKEN))
     if user is None \
             or (user.username != session.explorer.username and user.username != session.source.username):
         simple_response(ClientEvents.SESSION_CLOSE_ERROR, ResponseCode.FAILED,
@@ -258,7 +264,7 @@ def handle_disconnect():
 def simple_response(event, resultCode, message, sid):
     emit(
             event,
-            {"result": resultCode, "message": message},
+            {EventFields.RESULT: resultCode, EventFields.MESSAGE: message},
             room=sid
     )
 
@@ -267,5 +273,20 @@ def close_session(session):
     session.enabled = False
     session.closed = True
 
-    emit(ClientEvents.SESSION_CLOSED, {"session_id": session.uuid}, room=session.explorer.socket_id)
-    emit(ClientEvents.SESSION_CLOSED, {"session_id": session.uuid}, room=session.source.socket_id)
+    emit(ClientEvents.SESSION_CLOSED, {EventFields.SESSION_ID: session.uuid}, room=session.explorer.socket_id)
+    emit(ClientEvents.SESSION_CLOSED, {EventFields.SESSION_ID: session.uuid}, room=session.source.socket_id)
+
+
+class EventFields:
+    ANSWER = "answer"
+    RESULT = "result"
+    MESSAGE = "message"
+
+    TOKEN = "token"
+    SESSION_ID = "session_id"
+    SOURCE = "source"
+    EXPLORER = "explorer"
+
+    REQUEST_CODE = "request_code"
+    RESPONSE_DATA = "response_data"
+    PATH = "path"
