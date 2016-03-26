ce992789a3f2422d7c4ac630bb4ee01b76c29298
AmirHosein KazemNejad <ub.maka@gmail.com>
2016-03-26 17:58:15 +0430

    New config model

---
+++ gohappyserver/config_model.py
@@ -0,0 +1,7 @@
+db = {
+    "user": "root",
+    "password": "12345",
+}
+
+SECRET_KEY = 'W\xa9\xf4\x8b\x9aN\xecZCw\x020\xcdZp\x96.\x0f\xacq\xf1\xdf3\xc9'
+TOKEN_EXPIRATION_TIME = 150 * 24 * 60 * 60
